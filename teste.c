0ad395197ebcccd5b0c018238e0389e5d2b5de6c
Luiz0302 <luizgabrielfau@gmail.com>
2026-05-13 14:58:19 -0400

    Update teste.c

--- teste.c
+++ teste.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <locale.h>
 int main() {
-    
+    setlocale(LC_ALL, "portuguese");
+
     // informações baralho
     // Das 44 cartas do baralho, 26 são monstros (naipe preto), 9 armas (Ouro) e 9 poções (Copas)
     int baralho_total[44];
     int baralho_naipes[44]; 
 
     // informações player
-    int hp = 20;
+    int hp = 0;
+
+    if (hp <= 0)
+    {
+        printf("=========== Game Over! =========== \nVocê foi consumido pela masmorra \n");
+    }
+    
     return 0;
 }
