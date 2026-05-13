5a80bbbc1a5e94b8f5b13e46360498203fc5bdaf
Luiz0302 <luizgabrielfau@gmail.com>
2026-05-13 14:43:40 -0400

    Update teste.c

--- teste.c
+++ teste.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
 int main() {
     
+    // informações baralho
+    // Das 44 cartas do baralho, 26 são monstros (naipe preto), 9 armas (Ouro) e 9 poções (Copas)
+    int baralho_total[44];
+    int baralho_naipes[44]; 
+
+    // informações player
+    int hp = 20;
+    return 0;
 }
